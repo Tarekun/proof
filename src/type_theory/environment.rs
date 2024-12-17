58eef1352c68f295b0946bb4db96edeb33d4588c
Daniele Tarek Iaisy <extern.daniele.tarek.iaisy@lamborghini.com>
2024-12-17 14:30:34 +0100

    wip

--- src/type_theory/environment.rs
+++ src/type_theory/environment.rs
@@ -1,23 +1,43 @@
+
 use std::collections::HashMap;
 use crate::type_theory::stlc::StlcTerm;
 
+
 #[derive(Debug, Default)]
-pub struct Context {
-    pub variables: HashMap<String, StlcTerm>,
+pub struct Environment {
+    pub context: HashMap<String, StlcTerm>, //var_name, type
+    pub deltas: HashMap<String, StlcTerm>,  //var_name, term
 }
 
-impl Context {
+impl Environment {
     // Add a new variable to the context
-    pub fn add_variable(&mut self, name: String, term: StlcTerm) {
-        self.variables.insert(name, term);
+    pub fn add_variable_definition(&mut self, name: &str, term: StlcTerm) {
+        self.deltas.insert(name.to_string(), term);
     }
 
-    // Retrieve a variable from the context
-    pub fn get_variable(&self, name: &str) -> Option<&StlcTerm> {
-        self.variables.get(name)
+    pub fn add_variable_to_context(&mut self, name: &str, type_term: StlcTerm) {
+        self.context.insert(name.to_string(), type_term);
+    }
+
+    pub fn get_from_context(&self, name: &str) -> Option<(&str, &StlcTerm)> {
+        self.context.get(name).map(|type_term| (name, type_term))
+    }
+
+    pub fn get_from_deltas(&self, name: &str) -> Option<(&str, &StlcTerm)> {
+        self.deltas.get(name).map(|body| (name, body))
+        // match self.deltas.get(name) {
+        //     Some(body) => Some((name, body)),
+        //     None => None,
+        // }
     }
 
     pub fn is_bound(&self, name: &str) -> bool {
-        self.variables.contains_key(name)
+        match self.get_from_context(&name) {
+            Some(_) => true,
+            None => match self.get_from_deltas(&name) {
+                Some(_) => true,
+                None => false,
+            },
+        }
     }
 }
--- src/type_theory/stlc.rs
+++ src/type_theory/stlc.rs
@@ -2,9 +2,9 @@ use nom::Err;
 use std::collections::HashMap;
 
 use crate::parsing;
-use crate::type_theory::environment::Context;
+use crate::type_theory::environment::Environment;
 
-#[derive(Debug, PartialEq)] //support toString printing and equality check
+#[derive(Debug, PartialEq, Clone)] //support toString printing and equality check
 pub enum StlcTerm {
     Variable(String),
     Abstraction(String, Box<StlcTerm>),
@@ -14,37 +14,35 @@ pub enum StlcTerm {
 
 fn evaluate_ast_rec(
     ast: parsing::NsAst,
-    context: Context,
-) -> (Context, StlcTerm) {
+    mut environment: Environment,
+) -> (Environment, StlcTerm) {
     match ast {
         parsing::NsAst::Var(var_name) => {
-            //TODO check nell'environment, se presente ritorna il termine di definizione parsato
-            //check nel context, se presente ritorna il termine variabile
-            //altrimenti unbound
-            if context.is_bound(&var_name) {
-                (context, StlcTerm::Variable(var_name))
-            } else {
-                panic!("Unbound variable: {}", var_name)
+            match environment.get_from_deltas(&var_name) {
+                Some((var_name, body)) => {
+                    (environment, body.clone())
+                },
+                None => match environment.get_from_context(&var_name) {
+                    Some((_, type_term)) => (environment, StlcTerm::Variable(var_name)),
+                    None => panic!("Unbound variable: {}", var_name),
+                },
             }
         }
         parsing::NsAst::Abs(var_name, body) => {
-            let (mut context, body_term) =
-                evaluate_ast_rec(*body, context);
-            //TODO fix the context handling so bound variable without a body can still be recovered
-            //context.add_variable(var_name)
+            //TODO properly infer the type of the variable (and the function) instead of Unit
+            environment.add_variable_to_context(&var_name, StlcTerm::Unit);
+            let (mut environment, body_term) =
+                evaluate_ast_rec(*body, environment);
             (
-                context,
+                environment,
                 StlcTerm::Abstraction(var_name, Box::new(body_term)),
             )
         }
         parsing::NsAst::App(left, right) => {
-            //TODO questo è l'environment non il contesto
-            //il contesto è una lista di type judgement
-            //l'environment è una lista di costanti che se valutate sono riscritte con il corpo
-            let (context, left_term) = evaluate_ast_rec(*left, context);
-            let (context, right_term) = evaluate_ast_rec(*right, context);
+            let (environment, left_term) = evaluate_ast_rec(*left, environment);
+            let (environment, right_term) = evaluate_ast_rec(*right, environment);
             return (
-                context,
+                environment,
                 StlcTerm::Application(
                     Box::new(left_term),
                     Box::new(right_term),
@@ -52,15 +50,15 @@ fn evaluate_ast_rec(
             );
         }
         parsing::NsAst::Let(var_name, ast) => {
-            let (mut context, assigned_term) =
-                evaluate_ast_rec(*ast, context);
-            context.add_variable(var_name, assigned_term);
-            (context, StlcTerm::Unit)
+            let (mut environment, assigned_term) =
+                evaluate_ast_rec(*ast, environment);
+            environment.add_variable_definition(&var_name, assigned_term);
+            (environment, StlcTerm::Unit)
         }
         parsing::NsAst::Num(value) => panic!("non implemented"),
     }
 }
 
-pub fn evaluate_ast(ast: parsing::NsAst) -> (Context, StlcTerm) {
-    evaluate_ast_rec(ast, Context::default())
+pub fn evaluate_ast(ast: parsing::NsAst) -> (Environment, StlcTerm) {
+    evaluate_ast_rec(ast, Environment::default())
 }
