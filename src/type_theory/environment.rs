d0cc9ceba68e8c81729139a6ee339858970db8ec
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2024-12-21 12:15:55 +0100

    refactored to generic type theory and environment

--- src/type_theory/environment.rs
+++ src/type_theory/environment.rs
@@ -1,23 +1,31 @@
-use crate::type_theory::stlc::StlcTerm;
 use std::collections::HashMap;
 
-#[derive(Debug, Default, Clone)]
-pub struct Environment {
-    pub context: HashMap<String, StlcTerm>, //var_name, type
-    pub deltas: HashMap<String, StlcTerm>,  //var_name, term
+#[derive(Debug, Clone)]
+pub struct Environment<TermType> {
+    pub context: HashMap<String, TermType>, //var_name, type
+    pub deltas: HashMap<String, TermType>,  //var_name, term
 }
 
-impl Environment {
-    // Add a new variable to the context
-    pub fn add_variable_definition(&mut self, name: &str, term: &StlcTerm) {
+impl<TermType: Clone> Environment<TermType> {
+    pub fn new() -> Self {
+        Self {
+            context: HashMap::new(),
+            deltas: HashMap::new(),
+        }
+    }
+
+    /// Define a new variable adding it to the delta substitution context, along with
+    /// its definition body
+    pub fn add_variable_definition(&mut self, name: &str, term: &TermType) {
         //TODO avoid cloning?
         self.deltas.insert(name.to_string(), term.clone());
     }
 
+    /// Insert a new typed variable into the context
     pub fn add_variable_to_context(
         &mut self,
         name: &str,
-        type_term: &StlcTerm,
+        type_term: &TermType,
     ) {
         //TODO avoid cloning?
         self.context.insert(name.to_string(), type_term.clone());
@@ -26,14 +34,14 @@ impl Environment {
     pub fn get_from_context<'a>(
         &'a self,
         name: &'a str,
-    ) -> Option<(&'a str, &'a StlcTerm)> {
+    ) -> Option<(&'a str, &'a TermType)> {
         self.context.get(name).map(|type_term| (name, type_term))
     }
 
     pub fn get_from_deltas<'a>(
         &'a self,
         name: &'a str,
-    ) -> Option<(&'a str, &'a StlcTerm)> {
+    ) -> Option<(&'a str, &'a TermType)> {
         self.deltas.get(name).map(|body| (name, body))
     }
 }
--- src/type_theory/stlc.rs
+++ src/type_theory/stlc.rs
@@ -1,5 +1,6 @@
 use crate::parsing;
 use crate::type_theory::environment::Environment;
+use crate::type_theory::interface::TypeTheory;
 
 #[derive(Debug, PartialEq, Clone)] //support toString printing and equality check
 pub enum StlcTerm {
@@ -9,93 +10,106 @@ pub enum StlcTerm {
     Unit,
 }
 
-fn evaluate_expression(
-    ast: parsing::Expression,
-    mut environment: Environment,
-) -> (Environment, StlcTerm) {
-    match ast {
-        parsing::Expression::VarUse(var_name) => {
-            match environment.get_from_deltas(&var_name) {
-                //TODO should delta-reduce the variable here?
-                Some((var_name, _)) => (
-                    environment.clone(),
-                    StlcTerm::Variable(var_name.to_string()),
-                ),
-                None => match environment.get_from_context(&var_name) {
-                    Some((_, _)) => (environment, StlcTerm::Variable(var_name)),
-                    None => panic!("Unbound variable: {}", var_name),
-                },
-            }
-        }
-        parsing::Expression::Abstraction(var_name, body) => {
-            //TODO properly infer the type of the variable (and the function) instead of Unit
-            environment.add_variable_to_context(&var_name, &StlcTerm::Unit);
-            let (mut environment, body_term) =
-                evaluate_expression(*body, environment);
-            let function =
-                StlcTerm::Abstraction(var_name.clone(), Box::new(body_term));
-            environment.add_variable_to_context(&var_name, &StlcTerm::Unit);
+pub struct Stlc;
+impl TypeTheory for Stlc {
+    type Term = StlcTerm;
 
-            (environment, function)
-        }
-        parsing::Expression::Application(left, right) => {
-            let (environment, left_term) =
-                evaluate_expression(*left, environment);
-            let (environment, right_term) =
-                evaluate_expression(*right, environment);
-            (
-                environment,
-                StlcTerm::Application(
-                    Box::new(left_term),
-                    Box::new(right_term),
-                ),
-            )
-        }
-        _ => panic!("non implemented"),
-    }
-}
-
-fn evaluate_statement(
-    ast: parsing::Statement,
-    environment: Environment,
-) -> Environment {
-    match ast {
-        parsing::Statement::Comment() => environment,
-        parsing::Statement::FileRoot(_, asts) => {
-            let mut current_env = environment;
-
-            for sub_ast in asts {
-                match sub_ast {
-                    parsing::NsAst::Stm(stm) => {
-                        current_env = evaluate_statement(stm, current_env)
-                    }
-                    parsing::NsAst::Exp(exp) => {
-                        let (new_env, _) =
-                            evaluate_expression(exp, current_env);
-                        current_env = new_env;
-                    }
+    fn evaluate_expression(
+        ast: parsing::Expression,
+        mut environment: Environment<StlcTerm>,
+    ) -> (Environment<StlcTerm>, StlcTerm) {
+        match ast {
+            parsing::Expression::VarUse(var_name) => {
+                match environment.get_from_deltas(&var_name) {
+                    //TODO should delta-reduce the variable here?
+                    Some((var_name, _)) => (
+                        environment.clone(),
+                        StlcTerm::Variable(var_name.to_string()),
+                    ),
+                    None => match environment.get_from_context(&var_name) {
+                        Some((_, _)) => {
+                            (environment, StlcTerm::Variable(var_name))
+                        }
+                        None => panic!("Unbound variable: {}", var_name),
+                    },
                 }
             }
+            parsing::Expression::Abstraction(var_name, body) => {
+                //TODO properly infer the type of the variable (and the function) instead of Unit
+                environment.add_variable_to_context(&var_name, &StlcTerm::Unit);
+                let (mut environment, body_term) =
+                    Stlc::evaluate_expression(*body, environment);
+                let function = StlcTerm::Abstraction(
+                    var_name.clone(),
+                    Box::new(body_term),
+                );
+                environment.add_variable_to_context(&var_name, &StlcTerm::Unit);
 
-            current_env
-        }
-        parsing::Statement::Let(var_name, ast) => {
-            let (mut environment, assigned_term) =
-                evaluate_expression(*ast, environment);
-            environment.add_variable_definition(&var_name, &assigned_term);
-            environment
+                (environment, function)
+            }
+            parsing::Expression::Application(left, right) => {
+                let (environment, left_term) =
+                    Stlc::evaluate_expression(*left, environment);
+                let (environment, right_term) =
+                    Stlc::evaluate_expression(*right, environment);
+                (
+                    environment,
+                    StlcTerm::Application(
+                        Box::new(left_term),
+                        Box::new(right_term),
+                    ),
+                )
+            }
+            _ => panic!("non implemented"),
         }
     }
-}
 
-pub fn evaluate_ast(ast: parsing::NsAst) -> Environment {
-    match ast {
-        parsing::NsAst::Stm(stm) => {
-            evaluate_statement(stm, Environment::default())
+    fn evaluate_statement(
+        ast: parsing::Statement,
+        environment: Environment<StlcTerm>,
+    ) -> Environment<StlcTerm> {
+        match ast {
+            parsing::Statement::Comment() => environment,
+            parsing::Statement::FileRoot(_, asts) => {
+                let mut current_env = environment;
+
+                for sub_ast in asts {
+                    match sub_ast {
+                        parsing::NsAst::Stm(stm) => {
+                            current_env =
+                                Stlc::evaluate_statement(stm, current_env)
+                        }
+                        parsing::NsAst::Exp(exp) => {
+                            let (new_env, _) =
+                                Stlc::evaluate_expression(exp, current_env);
+                            current_env = new_env;
+                        }
+                    }
+                }
+
+                current_env
+            }
+            parsing::Statement::Let(var_name, ast) => {
+                let (mut environment, assigned_term) =
+                    Stlc::evaluate_expression(*ast, environment);
+                environment.add_variable_definition(&var_name, &assigned_term);
+                environment
+            }
         }
-        parsing::NsAst::Exp(exp) => {
-            let (new_env, _) = evaluate_expression(exp, Environment::default());
-            new_env
+    }
+
+    fn evaluate_ast(ast: parsing::NsAst) -> Environment<StlcTerm> {
+        match ast {
+            parsing::NsAst::Stm(stm) => {
+                Stlc::evaluate_statement(stm, Environment::<StlcTerm>::new())
+            }
+            parsing::NsAst::Exp(exp) => {
+                let (new_env, _) = Stlc::evaluate_expression(
+                    exp,
+                    Environment::<StlcTerm>::new(),
+                );
+                new_env
+            }
         }
     }
 }
