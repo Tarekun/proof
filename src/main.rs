8f83386fd68a1d49427654cc7de0f2966200d061
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2025-01-19 11:48:35 +0100

    fixed compiling

--- src/main.rs
+++ src/main.rs
@@ -42,16 +42,17 @@ fn main() {
     println!("Parsed AST: {:?}", ast);
     println!("Remaining input: '{}'\n", remaining);
 
+    //this is fixed in entrypoint branch
     // let environment = Stlc::elaborate_ast(ast);
-    let environment = Cic::elaborate_ast(ast);
-    for definition in environment.deltas {
-        let (var_name, term) = definition;
-        println!("defined term {:?}: {:?}", var_name, term);
-    }
+    // let environment = Cic::elaborate_ast(ast);
+    // for definition in environment.deltas {
+    //     let (var_name, term) = definition;
+    //     println!("defined term {:?}: {:?}", var_name, term);
+    // }
 
-    println!("\nContext:");
-    for assumption in environment.context {
-        let (var_name, var_type) = assumption;
-        println!("var {:?} : {:?}", var_name, var_type);
-    }
+    // println!("\nContext:");
+    // for assumption in environment.context {
+    //     let (var_name, var_type) = assumption;
+    //     println!("var {:?} : {:?}", var_name, var_type);
+    // }
 }
