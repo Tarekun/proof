d1a0ca7896250d237262e6ea13e113c8dbb88d3e
Daniele Tarek Iaisy <iaisy.danieletarek@gmail.com>
2025-02-08 18:08:14 +0100

    fix of type range of lambda abstraction

--- src/parser/expressions.rs
+++ src/parser/expressions.rs
@@ -35,7 +35,8 @@ pub fn parse_abs(input: &str) -> IResult<&str, Expression> {
     let (input, var_name) = preceded(multispace0, parse_identifier)(input)?;
     let (input, _) = preceded(multispace0, tag(":"))(input)?;
     //TODO should allow product type expressions here or only predefined type vars?
-    let (input, type_var) = preceded(multispace0, parse_var)(input)?;
+    let (input, type_var) =
+        preceded(multispace0, parse_type_expression)(input)?;
     let (input, _) = preceded(multispace0, char('.'))(input)?;
     let (input, body) = preceded(multispace0, parse_expression)(input)?;
 
